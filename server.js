6eb4c39449ec6a92a4be8c40db61ceef9715524e
John Paulo <johnpaulo0602@gmail.com>
2024-11-25 13:01:20 -0300

    gd

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
